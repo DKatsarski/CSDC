65a66dc0283710285c4d51b37cb1f91e5eb6ccf1
DKatsarski <dd.katsarskI@gmail.com>
2019-07-10 15:41:13 +0300

    Testing

---
+++ TestingPlace/Web/Web/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Web.Models.Test;
+
+namespace Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(TestModel testModel)
+        {
+            if (ModelState.IsValid)
+            {
+                return this.Json(testModel);
+
+            }
+            else
+            {
+                return this.View(testModel);
+            }
+        }
+    }
+}
---
+++ TestingPlace/Web/Web/Models/Test/MyAdditionalModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Models.Test
+{
+    public class MyAdditionalModel
+    {
+        public int ManyNumbers { get; set; }
+        public decimal ALotOfMoney { get; set; }
+    }
+}
