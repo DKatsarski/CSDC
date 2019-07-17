4be27d6d82e09fe43741e1645cf05fdba14f5682
DKatsarski <dd.katsarskI@gmail.com>
2019-07-17 12:16:27 +0300

    adding css file to jsx file

--- ReactComplete/react-complete-guide/src/Person/Person.js
+++ ReactComplete/react-complete-guide/src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>Lets check if {props.name} is {props.age} out with a {Math.floor(Math.random() * 30)} rnd value!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} />
