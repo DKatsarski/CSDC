b708f6724eadc3824d03aaa706bac3093fd30906
DKatsarski <dd.katsarskI@gmail.com>
2019-07-17 14:27:51 +0300

    dynamically changing the objects through input

--- ReactComplete/react-complete-guide/src/App.js
+++ ReactComplete/react-complete-guide/src/App.js
@@ -25,13 +25,22 @@ class App extends Component {
     this.setState({ showPersons: !doesShow });
   }
 
-  nameChangedHandler = (event) => {
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState({
-      persons: [
-        { id: 'dfa', name: "asd", age: 28 },
-        { id: 'sdffd', name: event.target.value, age: 32 },
-        { id: '23rf', name: 'AAAA', age: 52 }
-      ]
+      persons: persons
     })
   }
 
@@ -46,9 +55,10 @@ class App extends Component {
               click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age}
-              key={person.id} />
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
-      
+
         </div>
       );
     }
