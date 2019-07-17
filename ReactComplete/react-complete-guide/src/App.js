5319104c35f840ab30db5198126856ae88222055
DKatsarski <dd.katsarskI@gmail.com>
2019-07-17 12:53:25 +0300

    the proper way of dynamically showing info!!!

--- ReactComplete/react-complete-guide/src/App.js
+++ ReactComplete/react-complete-guide/src/App.js
@@ -9,7 +9,9 @@ class App extends Component {
       { name: 'Max', age: 28 },
       { name: 'ivan', age: 32 },
       { name: 'Stephi', age: 52 },
-    ]
+    ],
+    otherState: 'wtf',
+    showPersons: false
   }
 
   switchNameHandler = (newName) => {
@@ -23,6 +25,11 @@ class App extends Component {
     })
   }
 
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
   nameChangedHandler = (event) => {
     this.setState({
       animals: [
@@ -34,22 +41,33 @@ class App extends Component {
   }
 
   render() {
+let persons = null;
+
+if  (this.state.showPersons) {
+  persons = (
+    <div>
+    <Person
+      name={this.state.animals[0].name}
+      age={this.state.animals[0].age} />
+    <Person
+      name={this.state.animals[1].name}
+      age={this.state.animals[1].age}
+      click={this.switchNameHandler}
+      changed={this.nameChangedHandler}>SDFSDFSDF</Person>
+    <Person
+      name={this.state.animals[2].name}
+      age={this.state.animals[2].age} />
+  </div> 
+  );
+}
+
     return (
       <div className="App">
         <h1>hm...  its here</h1>
         <p>does this work</p>
-        <button onClick={this.switchNameHandler.bind(this, "GOSHKO")}>Switch name</button>
-        <Person
-          name={this.state.animals[0].name}
-          age={this.state.animals[0].age} />
-        <Person
-          name={this.state.animals[1].name}
-          age={this.state.animals[1].age}
-          click={this.switchNameHandler}
-          changed={this.nameChangedHandler}>SDFSDFSDF</Person>
-        <Person
-          name={this.state.animals[2].name}
-          age={this.state.animals[2].age} />
+        <button onClick={this.togglePersonsHandler}>Switch name</button>
+        
+       {persons}
 
       </div>
     );
