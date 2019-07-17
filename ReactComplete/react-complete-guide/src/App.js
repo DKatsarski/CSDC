e0259e77988b350c0ce85d489d19b8c51e671350
DKatsarski <dd.katsarskI@gmail.com>
2019-07-17 10:15:06 +0300

    creating funcion and using it

--- ReactComplete/react-complete-guide/src/App.js
+++ ReactComplete/react-complete-guide/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>hm... maybe its here</h1>
+        <h1>hm... maybeeee its here</h1>
+        <p>does this work</p>
+        <label>mmm</label>
+        <Person />
       </div>
     );
   }
---
+++ ReactComplete/react-complete-guide/src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = () => {
+    return <p>Lets check this out!</p>
+}
+
+export default person;
