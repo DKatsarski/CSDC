556e1e1752b9b66330d08f743b72ebac678f12dd
DKatsarski <dd.katsarskI@gmail.com>
2019-07-17 11:53:44 +0300

    setState explored

--- ReactComplete/react-complete-guide/src/App.js
+++ ReactComplete/react-complete-guide/src/App.js
@@ -6,25 +6,32 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     animals: [
-      { name: 'Max', age: 28},
-      { name: 'ivan', age: 32},
-      { name: 'Stephi', age: 52},
+      { name: 'Max', age: 28 },
+      { name: 'ivan', age: 32 },
+      { name: 'Stephi', age: 52 },
     ]
   }
 
   switchNameHandler = () => {
-    console.log('asdf');
+    // console.log('asdf');
+    this.setState({
+      animals: [
+        { name: 'asdfffd', age: 28 },
+        { name: 'ivan', age: 32 },
+        { name: 'AAAA', age: 52 }
+      ]
+    })
   }
 
   render() {
     return (
       <div className="App">
-        <h1>hm... maybeeee its here</h1>
+        <h1>hm...  its here</h1>
         <p>does this work</p>
         <button onClick={this.switchNameHandler}>Switch name</button>
-        <Person name={this.state.animals[0].name} age={this.state.animals[0].age}/>
+        <Person name={this.state.animals[0].name} age={this.state.animals[0].age} />
         <Person name={this.state.animals[1].name} age={this.state.animals[1].age}>SDFSDFSDF</Person>
-        <Person name={this.state.animals[2].name} age={this.state.animals[2].age}/>
+        <Person name={this.state.animals[2].name} age={this.state.animals[2].age} />
 
       </div>
     );
