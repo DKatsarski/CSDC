a936134965bed7c9fc53e46bd605a376986c8472
DKatsarski <dd.katsarskI@gmail.com>
2019-10-24 14:36:43 +0300

    little refactoring

--- ReactComplete/react-complete-guide/src/App.js
+++ ReactComplete/react-complete-guide/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
 
   deletePersonHandler = (personIndex) => {
     const persons = [...this.state.persons]
-    persons.splice(personIndex, 1);
+    persons.splice(personIndex, 1); 
     this.setState({ persons: persons });
   }
 
