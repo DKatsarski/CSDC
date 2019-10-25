4c45be7eab0075cec15daa7b5ee21ed9207f0907
DKatsarski <dd.katsarskI@gmail.com>
2019-10-25 14:52:51 +0300

    cockpit started

---
+++ ReactComplete/react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const cockpit = () => {
+    return (
+        <div>
+            <h1>first react sentance</h1>
+            <p className={classes.join(' ')}>asdfasdfasdfasdf</p>
+            <button
+                style={style}
+                onClick={this.togglePersonsHandler}>Switch Name
+          </button>
+        </div>
+    );
+};
+
+export default cockpit;
