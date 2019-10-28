ddf0600562b9962d159d09db84f366fc87cbc506
DKatsarski <dd.katsarskI@gmail.com>
2019-10-28 11:20:56 +0200

    small refactoring

--- ReactComplete/react-complete-guide/src/containers/App.js
+++ ReactComplete/react-complete-guide/src/containers/App.js
@@ -94,12 +94,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <h1>first react sentance</h1>
-        <p className={classes.join(' ')}>asdfasdfasdfasdf</p>
-        <button
-          style={style}
-          onClick={this.togglePersonsHandler}>Switch Name
-          </button>
+
         {persons}
 
       </div>
