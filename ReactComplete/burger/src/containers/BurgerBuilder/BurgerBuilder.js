deec630214f47578a424219a1a6f6a079e75ebe4
DKatsarski <dd.katsarskI@gmail.com>
2019-10-31 16:56:24 +0200

    adding ingredients is possible

--- ReactComplete/burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ ReactComplete/burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,21 +3,54 @@ import Auxilliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 import BuildControlls from '../../components/Burger/BuildControlls/BuildControlls';
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+
+};
+
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
-            salad: 2,
-            bacon: 2,
-            cheese: 1,
-            meat: 3
-        }
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        },
+        totalPrice: 4
     }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
+    }
+
+
+    RemoveIngredientHandler = (type) => {
+
+    }
+
+
+
+
+
 
     render() {
         return (
             <Auxilliary>
                 <Burger ingredients={this.state.ingredients} />
-                <BuildControlls />
+                <BuildControlls
+                    ingredientAdded={this.addIngredientHandler} />
             </Auxilliary>
         );
     }
