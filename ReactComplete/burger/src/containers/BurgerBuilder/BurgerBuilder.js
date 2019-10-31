5f76eaf386102822be0f6f529782b13472d49f96
DKatsarski <dd.katsarskI@gmail.com>
2019-10-31 16:02:50 +0200

    adjusting the burger accoring to the ingreadients

--- ReactComplete/burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ ReactComplete/burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,19 @@ import Auxilliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 2,
+            bacon: 2,
+            cheese: 1,
+            meat: 3
+        }
+    }
+
     render() {
         return (
             <Auxilliary>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build Controls</div>
             </Auxilliary>
         );
