27b03bcd58f686263e51b1d501457069672e9e88
DKatsarski <dd.katsarskI@gmail.com>
2019-11-01 17:05:08 +0200

    OrderSummary almost done -> some defects are present

--- ReactComplete/burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ ReactComplete/burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Auxilliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 import BuildControlls from '../../components/Burger/BuildControlls/BuildControlls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -78,6 +80,9 @@ class BurgerBuilder extends Component {
 
         return (
             <Auxilliary>
+                <Modal>
+                    <OrderSummary ingredients={this.state.ingredients} />
+                </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControlls
                     ingredientAdded={this.addIngredientHandler}
