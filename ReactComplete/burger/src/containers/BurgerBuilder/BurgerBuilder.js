21ffba88593aca3656521d6854167b1a7c1f9fc6
DKatsarski <dd.katsarskI@gmail.com>
2019-11-06 13:42:00 +0200

    before burgeris loaded error could't be catch with componentDidMount _> using WILL mount fixes that error. If "will mount" is not available due to updates, placeing the logic in the constructor will fix this error

--- ReactComplete/burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ ReactComplete/burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -23,13 +23,17 @@ class BurgerBuilder extends Component {
         totalPrice: 4,
         purchasable: false,
         purchasing: false,
-        loading: false
+        loading: false,
+        error: false
     }
 
     componentDidMount() {
         axios.get('https://react-my-burger-3a56c.firebaseio.com/ingredients.json')
             .then(response => {
                 this.setState({ ingredients: response.data })
+            })
+            .catch(error => {
+                this.setState({ error: true })
             });
     }
 
@@ -122,8 +126,8 @@ class BurgerBuilder extends Component {
         }
 
         let orderSummary = null;
-        let burger = <Spinner />;
-        
+        let burger = this.state.error ? <p>Ingredients can't be loaded!</p> : <Spinner />;
+
         if (this.state.ingredients) {
 
             burger = (
