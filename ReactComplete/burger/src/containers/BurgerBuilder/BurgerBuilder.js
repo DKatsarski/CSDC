4f140b3a42fa4f70b984965ae68340a735debb5c
DKatsarski <dd.katsarskI@gmail.com>
2019-11-08 17:31:41 +0200

    check out working -> should be sending to db

--- ReactComplete/burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ ReactComplete/burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -92,34 +92,14 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        // this.setState({ loading: true });
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: 'Dimitar Katsarski',
-        //         adress: {
-        //             street: 'Mladost 3',
-        //             zipCode: '1215',
-        //             country: 'Bulgaria'
-        //         },
-        //         email: 'email@test.com',
-        //         deliveryMethod: 'fasteset'
-        //     }
-        // }
-        // axios.post('/orders.json', order)0
-        //     .then(response => {
-        //         this.setState({ loading: false, purchasing: false });
-        //     })
-        //     .catch(error => {
-        //         this.setState({ loading: false, purchasing: false });
-        //     });
+
         const queryParams = [];
 
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
         }
 
+        queryParams.push('price=' + this.state.totalPrice)
         const queryString = queryParams.join('&');
         this.props.history.push({
             pathname: '/checkout',
